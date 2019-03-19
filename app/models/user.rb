d38093b8e982e1da9a7391708d577f9b90a69afd
sato akihide <aki@sato.local>
2019-03-19 12:53:00 +0900

    UserモデルValidation追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  validates :name , presence: true
+  validates :email , presence: true , length: {maximum:100} ,
+    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i } , uniqueness: true
+  before_validation(:email.downcase)
   has_secure_password
+  validates :password , presence: true , length: {minimum:6}
 end
