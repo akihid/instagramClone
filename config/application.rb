f691c76bc381128ba56b875ceda015a155d21177
sato akihide <aki@sato.local>
2019-03-19 10:57:47 +0900

    アプリ作成。config,readme編集

---
+++ config/application.rb
@@ -0,0 +1,24 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ICloneApp
+  class Application < Rails::Application
+    config.load_defaults 5.2
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+
+  end
+end
